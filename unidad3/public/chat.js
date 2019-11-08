dd02843f2e21ffc80565ec8be4b6764c24df8c11
Jesus Geovany Dzul Canul <geovany.jg14@gmail.com>
2019-11-08 12:12:21 -0600

    Montrando resultados

--- unidad3/public/chat.js
+++ unidad3/public/chat.js
@@ -22,15 +22,9 @@ message.addEventListener('keypress', function(){
 
 socket.on('chat:message', function(data){
     actions.innerHTML = '';
-    output.innerHTML +=  
-    `<p> 
-    <strong>${data.username}</strong>:${data.message}</p>
-   
-        `;
-   
-    var b = vocales(data.message);
+     var b = vocales(data.message);
         console.log(b + "numero de vocoles recibidas");
-   
+      
     var c = palabra(data.message);
         console.log(c + " Cantidad de palabra recibidas.");
     var d = numeros(data.message);
@@ -40,6 +34,16 @@ socket.on('chat:message', function(data){
     
     var a = fnvocal(data.message);
         console.log(a + "Palabras que finalizan con letras que no son vocal.");
+    output.innerHTML +=  
+    `<p><strong>${data.username}</strong>:${data.message}</p>
+     <p><strong>"Numero de vocoles recibidas</strong>:: ${b}</p>
+     <p><strong>"Cantidad de palabra recibidas</strong>:: ${c}</p>
+     <p><strong>"Cantidad de numeros contenidos en el mensaje enviado</strong>:: ${d}</p>
+     <p><strong>"Cantidad de palabras que inician con mayuscula</strong>:: ${m}</p>
+     <p><strong>"Palabras que finalizan con letras que no son vocal</strong>:: ${a}</p>
+        `;
+       
+   
 });
 
 socket.on('chat:typing', function(data){
@@ -98,7 +102,7 @@ function mayusculas(datas){
  //Expresion regulares
 function fnvocal(datas){
     // Palabras que finalizan con letras que no son vocal.
-     var fnvocall = /[aeiou]$/gm;
+     var fnvocall = /[^aeiou]$/gm;
      var conta = 0;
      var cadenas = datas.split(" ");
      for (let i = 0; i < cadenas.length; i++) {
